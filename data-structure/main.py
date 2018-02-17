60c12a43257213c4fcfb9ae09be67c1b49a50124
Karim Khamiss <karim.khamiss@gmail.com>
2018-02-17 11:48:51 +0200

    diagram

--- data-structure/main.py
+++ data-structure/main.py
@@ -1,10 +1,10 @@
 from Category import Category
-categorys = []
-category1 = Category("A",20)
-category2 = Category("A",0)
-categorys.append(category1)
-categorys.append(category2)
-category2.fill_field("male",">20")
-category2.fill_field("female","10 - 20")
-category2.fields[0].fill_constraints("adult",">20")
-print(category2.fields[0].constraints[0].name)
+category_list = []
+category1 = Category("Category #1")
+category2 = Category("Category #2")
+category2.fill_fields("Field #1", " 10 - 20 ")
+category2.fill_fields("Field #2", " 10 - 20 ")
+category_list.append(category1)
+category2.fields[0].fill_constraints("Constraint #1"," > 20 ")
+category2.fields[0].fill_constraints("Constraint #2"," < 20 ")
+category_list.append(category2)
