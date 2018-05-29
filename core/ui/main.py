93134919a3ec9700a031bb5d932bfe98f5bca19c
Karim <karim.khamiss@gmail.com>
2018-05-29 10:51:46 +0200

    Home Screen & Login Screen

--- core/ui/main.py
+++ core/ui/main.py
@@ -11,162 +11,22 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 
 from core.postprocessing.finalResult import getTestResult
-from core.postprocessing.postproccessing import Test
+# from core.postprocessing.postproccessing import Test
 from core.ui.navigationdrawer import NavigationDrawer
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.actionbar import ActionBar, ActionButton, ActionPrevious
 from kivy.properties import  ObjectProperty
-from core.classifier.prediction import output_his, prediction
+# from core.classifier.prediction import output_his, prediction
 
 Config.set('graphics', 'width', '440')
 Config.set('graphics', 'height', '620')
-### Menu ###
-SidePanel_AppMenu = {'Home':['Press_Home',None],
-                     'My Analysis':['Press_Analysis',None],
-                     'Profile':['Press_Profile',None],
-                     'Legal&Privacy':['Press_Legal',None],
-                     'Setting':['Press_Setting',None],
-                     }
-id_AppMenu_METHOD = 0
-id_AppMenu_PANEL = 1
-RootApp = None
-
-class SidePanel(BoxLayout):
-    pass
-
-class MenuItem(Button):
-    def __init__(self, **kwargs):
-        super(MenuItem, self).__init__( **kwargs)
-        self.bind(on_press=self.menuitem_selected)
-
-    def menuitem_selected(self, *args):
-        print (self.text, SidePanel_AppMenu[self.text], SidePanel_AppMenu[self.text][id_AppMenu_METHOD])
-        try:
-            function_to_call = SidePanel_AppMenu[self.text][id_AppMenu_METHOD]
-        except:
-            print ('error')
-            return
-        getattr( RootApp, function_to_call )()
-
-
-
-class AppActionBar(ActionBar):
-    pass
-
-class ActionMenu(ActionPrevious):
-    def menu(self):
-        print ('ActionMenu')
-        RootApp.toggle_sidepanel()
-
-
-
-class MainPanel(BoxLayout):
-    pass
-
-class AppArea(FloatLayout):
-    pass
-
-class CircularButton(ButtonBehavior, Widget):
-    pass
-
-class B_Home(FloatLayout):
-    pass
-
-class B_Analysis(FloatLayout):
-    pass
-
-class B_Profile(FloatLayout):
-    pass
-
-
-class B_Legal(FloatLayout):
-    pass
-
-
-class B_Setting(FloatLayout):
-    pass
-
-
-
-
-class NavDrawer(NavigationDrawer):
-    def __init__(self, **kwargs):
-        super(NavDrawer, self).__init__( **kwargs)
-
-    def close_sidepanel(self, animate=True):
-        if self.state == 'open':
-            if animate:
-                self.anim_to_state('closed')
-            else:
-                self.state = 'closed'
-
-# presentation = Builder.load_file("androidapp.kv")
-
-
-class Menu(App):
-
-    def build(self):
-        global RootApp
-        RootApp = self
-
-        # NavigationDrawer
-        self.navigationdrawer = NavDrawer()
-
-        # SidePanel
-        side_panel = SidePanel()
-        self.navigationdrawer.add_widget(side_panel)
-
-        # MainPanel
-        self.main_panel = MainPanel()
-        self.navigationdrawer.anim_type = 'slide_above_anim'
-        self.navigationdrawer.add_widget(self.main_panel)
-
-        return self.navigationdrawer
-
-    def toggle_sidepanel(self):
-        self.navigationdrawer.toggle_state()
-
-    def Press_Home(self):
-        print ('Home')
-
-    def Press_Analysis(self):
-        print ('Analysis')
-        self._switch_main_page('My Analysis', B_Analysis)
-
-
-    def Press_Profile(self):
-        print ('Profile')
-        self._switch_main_page('Profile',  B_Profile)
-
-
-    def Press_Legal(self):
-        print ('Legal')
-        self._switch_main_page('Legal&Privacy',  B_Legal)
-
-
-    def Press_Setting(self):
-        print ('Profile')
-        self._switch_main_page('Setting',  B_Setting)
-
-    def _switch_main_page(self, key,  panel):
-        self.navigationdrawer.close_sidepanel()
-
-        if not SidePanel_AppMenu[key][id_AppMenu_PANEL]:
-            SidePanel_AppMenu[key][id_AppMenu_PANEL] = panel()
-
-        main_panel = SidePanel_AppMenu[key][id_AppMenu_PANEL]
-        self.navigationdrawer.remove_widget(self.main_panel)
-        self.navigationdrawer.add_widget(main_panel)
-        self.main_panel = main_panel
-
-### Menu ###
 class Box(BoxLayout):
-    def predict(self):
-        List = []
-        List = output_his()  # return list of vectors in  each image but in list
-        linelist = prediction(List)
+    # def predict(self):
+    #     List = []
+    #     List = output_his()  # return list of vectors in  each image but in list
+    #     linelist = prediction(List)
     pass
 
 class Landing(App):
@@ -188,48 +48,6 @@ class SignUpApp(App):
 class Home(App):
     def build(self):
         return Box()
-class StudentListButton(ListItemButton):
-    selected_color = [0, 0, 0, 1]
-    deselected_color = [0, 0, 1, 1]
-    pass
-
-
-# Declare both screens
-class ResultScreen(Screen):
-    title = ObjectProperty()
-    first_list = ObjectProperty()
-    seoncd_list = ObjectProperty()
-    third_list = ObjectProperty()
-    def analysis(self):
-        List = []
-        List = output_his()  # return list of vectors in  each image but in list
-        linelist = prediction(List)
-        print(linelist)
-        counter = 0
-        self.title.text = linelist[0][0]
-        for test_name in linelist[0]:
-            counter = counter+1
-            if(counter == 1 or test_name == "not matched"):
-                continue
-            self.first_list.adapter.data.extend([test_name])
-            self.first_list._trigger_reset_populate()
-        for test_value in linelist[2]:
-            if (test_value == "none"):
-                continue
-            self.second_list.adapter.data.extend([test_value])
-            self.second_list._trigger_reset_populate()
-        feedback = getTestResult(linelist, 12, "male")
-        for test_feedback in feedback:
-            self.third_list.adapter.data.extend([test_feedback])
-            self.third_list._trigger_reset_populate()
-
-class ResultScreenApp(App):
-    def build(self):
-        # Create the screen manager
-        sm = ScreenManager()
-        sm.add_widget(ResultScreen(name='Result'))  # use name to move from screen to another
-        return sm
-
 if __name__ == '__main__':
     # List = []
     # item=["MCH","20","Clinical chemistry","يزيد عند مرض النقرص"]
@@ -239,6 +57,6 @@ if __name__ == '__main__':
     # Landing().run()
     # Home().run()
     # LoginApp().run()
-    # SignUpApp().run()
+    SignUpApp().run()
     # Menu().run()
-    ResultScreenApp().run()
+    # ResultScreenApp().run()
