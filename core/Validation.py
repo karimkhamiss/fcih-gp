be4e0f0cc84deb2cb51c423c083f412977c80d9e
AbdelrahmanAli10 <AbdelrahmanAli10@users.noreply.github.com>
2018-06-05 21:41:26 +0200

    Validation

--- core/Validation.py
+++ core/Validation.py
@@ -1,13 +1,30 @@
-import string
+import re
 
 
 class validation():
     def check_empty(sr):
         if sr == '':
-            print("Error")
-            return  True
+            return True
+            print("Erorr")
         else:
             return False
+            print("Done")
 
 
-        
+    def check_date(input_date):
+        match=re.match(r'(\d{2}/\d{2}/\d{4})',input_date)
+        if match== None:
+            return False
+            print("Error")
+        else:
+            return True
+            print("Done")
+
+    def check_names(input_str):
+        match=re.match(r'(^[a-zA-Z]+)?(^[a-zA-Z]+[0-9])',input_str)
+        if match== None:
+            return False
+            print("Error")
+        else:
+            return True
+            print("Done")
