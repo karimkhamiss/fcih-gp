70be76aa2fcc886f3aa5f94419ca9cfd861f7f8e
Hend Mohammad <hendmohammad45@gmail.com>
2018-06-21 01:05:20 +0200

    finalV1

--- core/postprocessing/finalResult.py
+++ core/postprocessing/finalResult.py
@@ -3,16 +3,18 @@ import pickle
 from core.postprocessing.postproccessing import Test
 def getTestResult(tests,age,gender):
     resultsDes=[]
+
     if gender == "male":
         for j in range(0,len(tests[1])):
-
+          i = 0
           testObj = tests[1][j]
           if testObj.name=="not matched":
 
               continue
           result = float(tests[2][j].replace(" ",'').replace("%",''))
           leng=len(testObj.male)-1
-          for i in range(0,leng):
+
+          while i<leng:
               if testObj.male[i][0]<= age and testObj.male[i][1] >=age:
                   if testObj.male[i+1][0]<= result and testObj.male[i+1][1] >=result:
                       resultsDes.append("Normal Range")
@@ -25,10 +27,11 @@ def getTestResult(tests,age,gender):
     else:
         for j in range(0,len(tests[1])):
             testObj=tests[1][j]
+            i = 0
             if testObj.name == "not matched":
                 continue
             result = float(tests[2][j].replace(" ",'').replace("%",''))
-            for i in range(0, len(testObj.female)-1):
+            while i <len(testObj.female)-1:
                 if testObj.female[i][0] <= age and testObj.female[i][1] >= age:
                     if testObj.female[i + 1][0] <= result and testObj.female[i + 1][1] >= result:
                         resultsDes.append("Normal Range")
